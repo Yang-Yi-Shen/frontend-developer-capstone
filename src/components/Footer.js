ec751c492d94c21246989372ecdec02224850b60
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-02-25 20:07:29 +0800

    Added footer logo + meta, fleshed out components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
 function Footer() {
   return (
     <footer>
-
+      <img src="../assets/Logo Vertical.png" alt="" />
+      <ul>
+        <li><h2>Doormat Navigation</h2></li>
+        <li>items</li>
+      </ul>
+      <ul>
+        <li><h2>Contact</h2></li>
+        <li>items</li>
+      </ul>
+      <ul>
+        <li><h2>Social Media Links</h2></li>
+      </ul>
     </footer>
   );
 }
