862a242ead822e81c2ba53d35d9145789e09227a
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-03-03 14:18:16 +0800

    Finished landing page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,28 @@
+import GreekSalad from '../assets/greek salad.jpg'
+
 function Main() {
     return (
       <main>
-        
+        <section id="specials">
+          <h2>Specials</h2>
+          <button>Online Menu</button>
+          <div className="specials-div">
+            <div className="specials-card">
+              <img src={GreekSalad} alt=""></img>
+              <h3>Greek Salad</h3>
+              <p>THe famous Greek salad of crispy lettuce, peppers, olive and our Chicago style feta cheese, garnished with crunchy garlic and rosemary crountons</p>
+              <b><p>Order a delivery</p></b>
+            </div>
+            <div className="specials-card">
+              <img src={GreekSalad} alt=""></img>
+              <h3>Greek Salad</h3>
+              <p>THe famous Greek salad of crispy lettuce, peppers, olive and our Chicago style feta cheese, garnished with crunchy garlic and rosemary crountons</p>
+              <b><p>Order a delivery</p></b>
+            </div>
+          </div>
+        </section>
+        <section id="testimonials"></section>
+        <section id="about"></section>
       </main>
     );
   }
