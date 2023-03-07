aad5b4b1f83a3b927dc2b845ea2acadd568879d4
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-03-07 15:06:36 +0800

    Added React Router

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,8 @@
 //IMPORTANT: think HARD about how availableTimes (line 12), updateTimes (line 44), initializeTimes (line 33) and useReducer fit together
 
 import { useState, useReducer } from 'react'
-import { fetchAPI, submitAPI } from 'fetchData'
+
+import { Routes, Route } from 'react-router-dom'
 
 import BookingForm from './BookingForm';
 import Hero from './Hero'
@@ -19,33 +20,71 @@ export default function Main() {
 
     return (
         <div className="main">
-            <Hero />
-            <Specials />
-            <Testimonials />
-            <BookingForm
-                selectedDate={selectedDate}
-                availableTimes={availableTimes}
-                handleDateChange={handleDateChange}
-            />
+            <Routes>
+                <Route path='/' element={
+                    <>
+                        <Hero />
+                        <Specials />
+                        <Testimonials />
+                    </>
+                } />
+                <Route path='/booking' element={
+                    <BookingForm
+                        selectedDate={selectedDate}
+                        availableTimes={availableTimes}
+                        handleDateChange={handleDateChange}
+                    />
+                } />
+            </Routes>
         </div>
     );
 }
 
+//code from coursera
+const seededRandom = function (seed) {
+    var m = 2**35 - 31;
+    var a = 185852;
+    var s = seed % m;
+    return function () {
+        return (s = s * a % m) / m;
+    };
+}
+
+const fetchAPI = function(date) {
+    let result = [];
+    let random = seededRandom(date.getDate());
+
+    for(let i = 17; i <= 23; i++) {
+        if(random() < 0.5) {
+            result.push(i + ':00');
+        }
+        if(random() < 0.5) {
+            result.push(i + ':30');
+        }
+    }
+    return result;
+};
+// const submitAPI = function(formData) {
+//     return true;
+// };
+//end of coursera code
+
 export function initializeTimes() {
-    return [
-        { time: "17:00", available: true },
-        { time: "18:00", available: true },
-        { time: "19:00", available: true },
-        { time: "20:00", available: true },
-        { time: "21:00", available: true },
-        { time: "22:00", available: true },
-    ];
+    const times = fetchAPI(new Date())
+    
+    const result = times.map((time) => ({
+        time: time,
+        available: true
+    }));
+
+    console.log(result)
+    return result;
 }
 
 export function updateTimes(state, action) {
     switch (action.type) {
         case "UPDATE_TIMES":
-            return fetchAPI(new Date());
+            return initializeTimes();
         default:
             return state;
     }
